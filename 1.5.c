7b043fa7bed45aaec358d9c522f1f34673b6d771
Darshana R <jaidarsh1983@gmail.com>
2025-12-17 23:51:55 +0530

    Delete 1.5.c

--- 1.5.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void main()
-{
-  int fahr;
-  printf("Fahrenheit Celsius\n");
-  for(fahr = 300; fahr >=0; fahr = fahr - 20)
-    printf("%5d %10.1f\n", fahr, (5.0/9.0)*(fahr-32));
-}
