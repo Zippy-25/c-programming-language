a15593cd4ec8317e9885be74f069d68410cb7bc6
Darshana R <jaidarsh1983@gmail.com>
2026-01-12 20:19:08 +0530

    Update 6.6.c

--- 12.01.26/6.6.c
+++ 12.01.26/6.6.c
@@ -1,3 +1,7 @@
+/*Exercise 6-6. Implement a simple version of the #define processor (i.e., no arguments)
+suitable for use with C programs, based on the routines of this section. You may also find
+getch and ungetch helpful.*/
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
