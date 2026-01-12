5f5c0cdc9cdc3c55c51a5d132be6eb981bdab9de
Darshana R <jaidarsh1983@gmail.com>
2026-01-12 20:18:10 +0530

    Add comment for exercise 6-5
    Add a comment for exercise 6-5 regarding the undef function.

--- 12.01.26/6.5.c
+++ 12.01.26/6.5.c
@@ -1,3 +1,6 @@
+/*Exercise 6-5. Write a function undef that will remove a name and definition from the table
+maintained by lookup and install.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
